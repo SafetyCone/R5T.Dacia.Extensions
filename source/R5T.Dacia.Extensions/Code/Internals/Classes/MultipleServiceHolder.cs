b7a62f77a3f3101ef4d226641cb327f6908982dc
MinexAutomation <minex.automation@gmail.com>
2019-12-21 23:09:36 -0800

    Added multiple service holder.

---
+++ source/R5T.Dacia.Extensions/Code/Internals/Classes/MultipleServiceHolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Dacia.Internals
+{
+    public class MultipleServiceHolder<TService> : IMultipleServiceHolder<TService>
+    {
+        public TService Value { get; }
+
+
+        public MultipleServiceHolder(TService value)
+        {
+            this.Value = value;
+        }
+    }
+}
---
+++ source/R5T.Dacia.Extensions/Code/Internals/Interfaces/IMultipleServiceHolder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Dacia.Internals
+{
+    /// <summary>
+    /// A interface for a holder service that helps with multiple services.
+    /// </summary>
+    public interface IMultipleServiceHolder<out TService>
+    {
+        TService Value { get; }
+    }
+}
