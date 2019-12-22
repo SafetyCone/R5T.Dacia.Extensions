cd36da86baea9ec023b3bc694c5302c9b2ab34d1
MinexAutomation <minex.automation@gmail.com>
2019-12-21 23:02:53 -0800

    Added SubTree() IServiceCollection extension.

---
+++ source/R5T.Dacia.Extensions/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Dacia.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection SubTree(this IServiceCollection services, Action<IServiceCollection> action)
+        {
+            action(services);
+
+            return services;
+        }
+    }
+}
