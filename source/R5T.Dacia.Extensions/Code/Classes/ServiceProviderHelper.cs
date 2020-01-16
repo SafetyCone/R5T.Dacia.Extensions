d154b72740468ba596a46c139e1600248f74d999
MinexAutomation <minex.automation@gmail.com>
2020-01-16 14:08:05 -0800

    Improved ServiceProviderHelper.

--- source/R5T.Dacia.Extensions/Code/Classes/ServiceProviderHelper.cs
+++ source/R5T.Dacia.Extensions/Code/Classes/ServiceProviderHelper.cs
@@ -5,8 +5,40 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace R5T.Dacia
 {
-    public static class ServiceProviderHelper
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// The <see cref="ServiceProviderHelper"/> class is instantiable to allow it to have extension methods in client libraries.
+    /// </remarks>
+    public class ServiceProviderHelper
     {
+        #region Static
+
+        /// <summary>
+        /// Allows syntactic-sugar of starting a code line with <see cref="ServiceProviderHelper"/>...
+        /// </summary>
+        public static ServiceProviderHelper New()
+        {
+            var serviceProviderHelper = new ServiceProviderHelper();
+            return serviceProviderHelper;
+        }
+
+        /// <summary>
+        /// A method that performs no action on an <see cref="IServiceCollection"/> instance.
+        /// Useful for when a null-operation is required in creating a service provider.
+        /// </summary>
+        public static IServiceCollection DoNothingAction(IServiceCollection services)
+        {
+            // Do nothing.
+
+            return services;
+        }
+
+        /// <summary>
+        /// Gets an empty service provider.
+        /// Useful for when a configuration service provider is required, but configuration of the configuration requires no services.
+        /// </summary>
         public static IServiceProvider GetEmptyServiceProvider()
         {
             var emptyServiceProvider = new ServiceCollection()
@@ -14,5 +46,7 @@ namespace R5T.Dacia
 
             return emptyServiceProvider;
         }
+
+        #endregion
     }
 }
