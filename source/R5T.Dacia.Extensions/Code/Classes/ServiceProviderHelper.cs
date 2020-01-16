4f1cceb59f41bbad88d74279cb866b09219556e3
MinexAutomation <minex.automation@gmail.com>
2020-01-16 12:13:20 -0800

    Added ServiceProviderHelper class. Added ServiceProviderHelper.GetEmptyServiceProvider().

---
+++ source/R5T.Dacia.Extensions/Code/Classes/ServiceProviderHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Dacia
+{
+    public static class ServiceProviderHelper
+    {
+        public static IServiceProvider GetEmptyServiceProvider()
+        {
+            var emptyServiceProvider = new ServiceCollection()
+                .BuildServiceProvider();
+
+            return emptyServiceProvider;
+        }
+    }
+}
