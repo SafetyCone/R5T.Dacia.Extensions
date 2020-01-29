987d0ba8a1207058b13382c2c4faa6d84a33a298
MinexAutomation <minex.automation@gmail.com>
2020-01-29 00:21:09 -0800

    Added ServiceAction<T> and DoNothingServiceAction class types.

---
+++ source/R5T.Dacia.Extensions/Code/Service Actions/DoNothingServiceAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace R5T.Dacia
+{
+    public class DoNothingServiceAction
+    {
+        public static Action DoNothingAction { get; } = () => { };
+
+
+        // Does not work: "The type or namespace 'T' could not be found ...".
+        // See: https://social.msdn.microsoft.com/Forums/en-US/e591d550-4df9-49e9-a5a1-3e28d602e263/introduce-generic-parameter-in-explicit-type-conversion?forum=csharplanguage
+        // Instead, put operator on ServiceAction<T>!
+        //public static implicit operator ServiceAction<T>(DoNothingServiceAction doNothingServiceAction)
+        //{
+        //    var serviceAction = new ServiceAction<T>(DoNothingServiceAction.DoNothingAction);
+        //    return serviceAction;
+        //}
+    }
+}
---
+++ source/R5T.Dacia.Extensions/Code/Service Actions/ServiceAction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace R5T.Dacia
+{
+    public static class ServiceAction
+    {
+        public static DoNothingServiceAction AddedElsewhere { get; } = new DoNothingServiceAction();
+        public static DoNothingServiceAction AlreadyAdded { get; } = new DoNothingServiceAction();
+        //public static DoNothingServiceAction AddedElsewhere { get; } = new DoNothingServiceAction();
+    }
+
+
+    public class ServiceAction<T>
+    {
+        private Action Action { get; }
+
+
+        public static implicit operator ServiceAction<T>(DoNothingServiceAction doNothingServiceAction)
+        {
+            var serviceAction = new ServiceAction<T>(DoNothingServiceAction.DoNothingAction);
+            return serviceAction;
+        }
+
+
+        public ServiceAction(Action action)
+        {
+            this.Action = action;
+        }
+
+        public void Run()
+        {
+            this.Action();
+        }
+    }
+}
