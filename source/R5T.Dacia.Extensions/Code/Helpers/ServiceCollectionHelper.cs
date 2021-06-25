04dde3a96d0aaee291749b2742f48b5ee77c01d5
David Coats <david@getsafetycone.com>
2021-06-24 18:59:38 -0700

    Add service collection helper methods.

--- source/R5T.Dacia.Extensions/Code/Helpers/ServiceCollectionHelper.cs
+++ source/R5T.Dacia.Extensions/Code/Helpers/ServiceCollectionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,9 +12,67 @@ namespace R5T.Dacia
         /// A method that performs no action on an <see cref="IServiceCollection"/> instance.
         /// Useful for when a null-operation is required in creating a service provider.
         /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
         public static void DoNothing(IServiceCollection services)
         {
             // Do nothing.
+        }
+
+        public static async Task<ServiceCollection> ConfigureServices(ServiceCollection services, Func<ServiceCollection, Task> configureServicesAction)
+        {
+            await configureServicesAction(services);
+
+            return services;
+        }
+
+        public static Task<ServiceCollection> ConfigureServices(Func<ServiceCollection, Task> configureServicesAction)
+        {
+            var services = new ServiceCollection();
+
+            return ServiceCollectionHelper.ConfigureServices(services, configureServicesAction);
+        }
+
+        public static ServiceCollection ConfigureServicesSynchronous(ServiceCollection services, Action<ServiceCollection> configureServicesAction)
+        {
+            configureServicesAction(services);
+
+            return services;
+        }
+
+        public static ServiceCollection ConfigureServicesSynchronous(Action<ServiceCollection> configureServicesAction)
+        {
+            var services = new ServiceCollection();
+
+            configureServicesAction(services);
+
+            return services;
+        }
+
+        public static T GetInstanceOfType<T>(ServiceCollection services)
+            where T : class
+        {
+            services.AddSingleton<T>();
+
+            var output = services.GetIntermediateRequiredService<T>();
+            return output;
+        }
+
+        public static T GetInstanceOfTypeSynchronous<T>(Action<ServiceCollection> configureServices)
+            where T : class
+        {
+            var services = ServiceCollectionHelper.ConfigureServicesSynchronous(configureServices);
+
+            var output = ServiceCollectionHelper.GetInstanceOfType<T>(services);
+            return output;
+        }
+
+        public static async Task<T> GetInstanceOfType<T>(Func<ServiceCollection, Task> configureServices)
+            where T: class
+        {
+            var services = await ServiceCollectionHelper.ConfigureServices(configureServices);
+
+            var output = ServiceCollectionHelper.GetInstanceOfType<T>(services);
+            return output;
         }
     }
 }
